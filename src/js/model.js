0771f832df03046772bea48c5b4a2c65a12297b6
Fuga14 <vladyslavpereder@gmail.com>
2023-02-16 16:00:13 +0200

    'pagination'

---
+++ src/js/model.js
@@ -0,0 +1,64 @@
+import { async } from 'regenerator-runtime';
+import { API_URL, RES_PER_PAGE } from './config';
+import { getJSON } from './helpers';
+
+export const state = {
+  recipe: {},
+  search: {
+    query: '',
+    results: [],
+    page: 1,
+    resultsPerPage: RES_PER_PAGE,
+  },
+};
+
+export const loadRecipe = async function (id) {
+  try {
+    const data = await getJSON(`${API_URL}${id}`);
+
+    const { recipe } = data.data;
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log(state.recipe);
+  } catch (err) {
+    // Temp error handling
+    console.error(`${err} 👁️👁️👁️`);
+    throw err;
+  }
+};
+
+export const loadSearchResults = async function (query) {
+  try {
+    state.search.query = query;
+    const data = await getJSON(`${API_URL}?search=${query}`);
+    const { recipes } = data.data;
+    state.search.results = recipes.map(rec => {
+      return {
+        id: rec.id,
+        title: rec.title,
+        publisher: rec.publisher,
+        image: rec.image_url,
+      };
+    });
+    console.log(state.search);
+  } catch (err) {
+    console.error(`${err} 👁️👁️👁️`);
+    throw err;
+  }
+};
+
+export const getSearchResultsPage = function (page = state.search.page) {
+  state.search.page = page;
+
+  const start = (page - 1) * state.search.resultsPerPage;
+  const end = page * state.search.resultsPerPage;
+  return state.search.results.slice(start, end);
+};
